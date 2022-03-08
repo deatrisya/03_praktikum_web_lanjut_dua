260aed49cce872edb86a291e54019af82ba502eb
deatrisya <mirelldee47@gmail.com>
2022-03-08 15:36:06 +0700

    add call food seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // \App\Models\Menu::factory(10)->create();
+        $this->call(FoodMenuSeeder::class);
     }
 }
